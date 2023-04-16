b9e8a6f82e6532a962628b94338ef91f8ff679a2
ElizabethProkh <liza.prokhorova.1999@mail.ru>
2023-04-16 11:49:54 +0300

    commit3

--- src/main/java/web/Model/User.java
+++ src/main/java/web/Model/User.java
@@ -1,16 +1,10 @@
 package web.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -26,4 +20,54 @@ public class User {
     @Column (name = "department")
     private String department;
 
+    public User() {
+    }
+
+    public User(String name, String surname, String department) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
--- src/main/java/web/Service/UserServiceImpl.java
+++ src/main/java/web/Service/UserServiceImpl.java
@@ -9,33 +9,38 @@ import web.Model.User;
 import java.util.List;
 
 @Service
-@Transactional
+
 public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserDao userDao;
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
 
     @Override
+    @Transactional
     public User getUser(int id) {
         return userDao.getUser(id);
     }
 
     @Override
+    @Transactional
     public void updateUser(int id, User updatedUser) {
         userDao.updateUser(id, updatedUser);
     }
 
     @Override
+    @Transactional
     public void deleteUser(int id) {
         userDao.deleteUser(id);
     }
